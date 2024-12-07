7a7277c58197e16bd900adf9b111fa64b797ee6b
Qar-Raz <q.raza.27140@khi.iba.edu.pk>
2024-12-07 10:54:33 +0500

    order placement completed

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -6,26 +6,43 @@ import db from '@/db/drizzle'
 import { sql } from 'drizzle-orm'
 
 export async function getLatestProducts() {
-  const query = sql`SELECT * FROM product ORDER BY "createdAt" DESC LIMIT 4;`
+  const query = sql`
+  SELECT *
+   FROM product
+    ORDER BY "createdAt"
+     DESC;`
   const data = await db.execute(query)
   return data.rows
 }
 
 export async function getExpensiveProducts() {
-  const query = sql`SELECT * FROM product ORDER BY "price" DESC LIMIT 4;`
+  const query = sql`
+  SELECT * 
+  FROM product
+   ORDER BY "price"
+    DESC;`
   const data = await db.execute(query)
   return data.rows
 }
 
 export async function getHighlyRatedProducts() {
-  const query = sql`SELECT * FROM product ORDER BY "rating" DESC LIMIT 4;`
+  const query = sql`
+  SELECT *
+   FROM product
+    ORDER BY "rating"
+     DESC;`
   const data = await db.execute(query)
   return data.rows
 }
 
 // this function will return a product by its slug if it is equal to the slug passed in the function --@Qamar
 export async function getProductBySlug(slug: string) {
-  const query = sql`SELECT * FROM product WHERE slug = ${slug} LIMIT 1;`
+  const query = sql`
+  SELECT * 
+  FROM product
+   WHERE slug
+    = ${slug}
+     LIMIT 1;`
   const result = await db.execute(query)
   return result.rows[0]
 }
--- lib/sample-data.ts
+++ lib/sample-data.ts
@@ -20,6 +20,41 @@ const sampleData = {
 
   products: [
     {
+      name: 'Black Coffee Mug',
+      slug: 'black-coffee-mug',
+      category: 'Home & Kitchen',
+      images: [
+        '/assets/images/coffee-mug-black.jpeg',
+        '/assets/images/coffee-mug-black.jpeg',
+      ],
+      price: '1000',
+      brand: 'KitchenAid',
+      rating: '3.9',
+      numReviews: 50,
+      stock: 100,
+      description:
+        'Enjoy your favorite beverage in this sleek black coffee mug, perfect for any occasion.',
+      isFeatured: false,
+      banner: '/assets/images/coffee-mug-black.jpeg',
+    },
+
+    {
+      name: 'iPhone 15 Pro Max',
+      slug: 'iphone-15-pro-max',
+      category: 'Electronics',
+      images: ['/assets/images/ipho.jpeg', '/assets/images/ipho.jpeg'],
+      price: '520000',
+      brand: 'Apple',
+      rating: '4.8',
+      numReviews: 150,
+      stock: 3,
+      description:
+        'Experience the latest technology with the new iPhone 16 Pro Max, featuring a stunning display and advanced camera system.',
+      isFeatured: true,
+      banner: '/assets/images/ipho.jpeg',
+    },
+
+    {
       name: 'Umar',
       slug: 'umar-hussain-blue-shirt',
       category: "Men's Shirt",
@@ -35,11 +70,27 @@ const sampleData = {
       banner: '/assets/images/banner-1.jpeg',
     },
     {
+      name: 'PlayStation 5 Pro',
+      slug: 'playstation-5-pro',
+      category: 'Gaming Consoles',
+      images: ['/assets/images/ps5-pro.jpeg', '/assets/images/ps5-pro.jpeg'],
+      price: '210000',
+      brand: 'Sony',
+      rating: '4.7',
+      numReviews: 200,
+      stock: 5,
+      description:
+        'Take your gaming experience to the next level with the PlayStation 5 Pro, offering enhanced graphics and performance.',
+      isFeatured: true,
+      banner: '/assets/images/ps5-pro.jpeg',
+    },
+
+    {
       name: 'LACOSTE TENNIS X NOVAK DJOKOVIC SPORTSUIT JACKET',
       slug: 'lacoste-tennis-x-novak-djokovic-sportsuit-jacket',
       category: "Men's Sweatshirts",
       images: ['/assets/images/p2-1.jpeg', '/assets/images/p2-2.jpeg'],
-      price: '199.90',
+      price: '12000',
       brand: 'Lacoste',
       rating: '4.2',
       numReviews: 8,
@@ -54,7 +105,7 @@ const sampleData = {
       slug: 'showerproof-sportsuit-track-pants',
       category: 'Track Pants',
       images: ['/assets/images/p3-1.jpeg', '/assets/images/p3-2.jpeg'],
-      price: '149.95',
+      price: '16000',
       brand: 'Nike',
       rating: '4.9',
       numReviews: 3,
@@ -67,7 +118,7 @@ const sampleData = {
       slug: 'mens-lacoste-sport-french-capsule-tracksuit-pants',
       category: 'Track Pants',
       images: ['/assets/images/p4-1.jpeg', '/assets/images/p4-2.jpeg'],
-      price: '125.95',
+      price: '21000',
       brand: 'Lacoste',
       rating: '3.6',
       numReviews: 5,
